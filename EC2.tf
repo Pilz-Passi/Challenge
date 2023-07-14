2e670b0e88659989a1d34ed7a612866ff165d4cc
Pascal Spielvogel <pascal-spielvogel@gmx.de>
2023-07-14 11:32:04 +0200

    changed EC2 setup

--- EC2.tf
+++ EC2.tf
@@ -1,11 +1,11 @@
-resource "aws_instance" "deham6demo1"{
+resource "aws_instance" "Wordpress-instance"{
     ami = "ami-0ae49954dfb447966"
     instance_type = "t3.micro"
     key_name = "vockey"
-    vpc_security_group_ids = [aws_security_group.devVPC_sg_allow_ssh_http.id]
-    subnet_id = aws_subnet.devVPC_public_subnet.id
+    vpc_security_group_ids = [aws_security_group.devVPC_sg_allow_http.id]
+    subnet_id = aws_subnet.devVPC_public_subnet1.id
     tags = {
-        Name = "deham6demo1"
+        Name = "Wordpress-instance"
     }
     provisioner "local-exec"{
     command = "echo Instance Type=${self.instance_type},Instance ID=${self.id},Public DNS=${self.public_dns},AMI ID=${self.ami} >> allinstancedetails"
