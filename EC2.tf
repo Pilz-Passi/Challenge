b20db38f5666ae5871bcb52c332d1a30e54eb284
Pascal Spielvogel <pascal-spielvogel@gmx.de>
2023-07-14 16:30:38 +0200

    changed user data / path

--- EC2.tf
+++ EC2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "Wordpress-instance"{
     key_name = "vockey"
     vpc_security_group_ids = [aws_security_group.devVPC_sg_allow_http.id]
     subnet_id = aws_subnet.devVPC_public_subnet1.id
-    user_data = data.template_file.init.rendered
+    user_data = "${file("init.tpl")}"
     tags = {
         Name = "Wordpress-instance"
     }
@@ -27,11 +27,5 @@ resource "aws_instance" "Wordpress-instance"{
     }*/
     provisioner "local-exec"{
     command = "echo Instance Type=${self.instance_type},Instance ID=${self.id},Public DNS=${self.public_dns},AMI ID=${self.ami} >> allinstancedetails"
-  }
-}
-data "template_file" "init" {
-  template = "${file("${path.module}/init.tpl")}"
-  vars = {
-    consul_address = "${aws_instance.consul.private_ip}"
   }
 }
