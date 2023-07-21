906b3528c0e58a57dbf4ea6cf9153052ede24f44
Pascal Spielvogel <pascal-spielvogel@gmx.de>
2023-07-21 11:36:42 +0200

    1st draft in pseudo-code

---
+++ create-ami.tf
@@ -0,0 +1,9 @@
+# Has to be delayed for at least 10 Minutes in order to give the original EC2 instance
+# a change to initialize and install the web server
+resource "aws_ami_from_instance" "worpress-image" {
+  name               = "terraform-wordpress-instance"
+  #create a variable, feeding from the output
+  source_instance_id = "i-xxxxxxxx" /*output -raw "instance_id" {
+  value       = aws_instance.Wordpress-instance.id
+    } OR $instance-id*/
+}
