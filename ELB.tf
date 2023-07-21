60371ca6cd3c3eed31dd7cd1aaeae24b846e2946
Pascal Spielvogel <pascal-spielvogel@gmx.de>
2023-07-21 15:56:53 +0200

    dunno

--- ELB.tf
+++ ELB.tf
@@ -1,6 +1,3 @@
-# data "aws_launch_template" "default" {
-#   name = "my-launch-template"
-# }
 # resource "aws_launch_configuration" "nginx_launch_config"{
 #     image_id = data.aws_ami.packeramisnginx.id
 #     instance_type = var.instance_type
@@ -54,24 +51,5 @@ resource "aws_lb" "test" {
 
   tags = {
     Environment = "production"
-  }
-}
-resource "aws_launch_template" "get_ready" {
-  name_prefix   = "get_ready"
-  image_id      = "ami-1a2b3c"
-  instance_type = "t2.micro"
-}
-data "aws_autoscaling_group" "scale_group1" {
-  name = "scale_group1"
-}
-resource "aws_autoscaling_group" "scale_group1" {
-  availability_zones = ["us-west-2"]
-  desired_capacity   = 1
-  max_size           = 5
-  min_size           = 2
-
-  launch_template {
-    id      = aws_launch_template.get_ready.id
-    version = "$Latest"
   }
 }
