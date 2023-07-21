a68212fe0af21e15da803dbf3e624ad7246bb156
Pascal Spielvogel <pascal-spielvogel@gmx.de>
2023-07-21 16:37:09 +0200

    deleted lines, that I think I don't need

--- Launch-template.tf
+++ Launch-template.tf
@@ -1,51 +1,51 @@
 resource "aws_launch_template" "foo" {
   name = "foo"
 
-  block_device_mappings {
-    device_name = "/dev/sdf"
+#   block_device_mappings {
+#     device_name = "/dev/sdf"
 
-    ebs {
-      volume_size = 20
-    }
-  }
+#     ebs {
+#       volume_size = 20
+#     }
+#   }
 
-  capacity_reservation_specification {
-    capacity_reservation_preference = "open"
-  }
+#   capacity_reservation_specification {
+#     capacity_reservation_preference = "open"
+#   }
 
-  cpu_options {
-    core_count       = 4
-    threads_per_core = 2
-  }
+#   cpu_options {
+#     core_count       = 4
+#     threads_per_core = 2
+#   }
 
-  credit_specification {
-    cpu_credits = "standard"
-  }
+#   credit_specification {
+#     cpu_credits = "standard"
+#   }
 
-  disable_api_stop        = true
-  disable_api_termination = true
+#   disable_api_stop        = true
+#   disable_api_termination = true
 
-  ebs_optimized = true
+#   ebs_optimized = true
 
-  elastic_gpu_specifications {
-    type = "test"
-  }
+#   elastic_gpu_specifications {
+#     type = "test"
+#   }
 
-  elastic_inference_accelerator {
-    type = "eia1.medium"
-  }
+#   elastic_inference_accelerator {
+#     type = "eia1.medium"
+#   }
 
-  iam_instance_profile {
-    name = "test"
-  }
+#   iam_instance_profile {
+#     name = "test"
+#   }
 
   image_id = data.aws_ami.amzLinux.id
 
   instance_initiated_shutdown_behavior = "terminate"
 
-  instance_market_options {
-    market_type = "spot"
-  }
+#   instance_market_options {
+#     market_type = "spot"
+#   }
 
   instance_type = "t2.micro"
 
@@ -57,20 +57,20 @@ resource "aws_launch_template" "foo" {
 #     license_configuration_arn = "arn:aws:license-manager:eu-west-1:123456789012:license-configuration:lic-0123456789abcdef0123456789abcdef"
 #   }
 
-  metadata_options {
-    http_endpoint               = "enabled"
-    http_tokens                 = "required"
-    http_put_response_hop_limit = 1
-    instance_metadata_tags      = "enabled"
-  }
+#   metadata_options {
+#     http_endpoint               = "enabled"
+#     http_tokens                 = "required"
+#     http_put_response_hop_limit = 1
+#     instance_metadata_tags      = "enabled"
+#   }
 
   monitoring {
     enabled = true
   }
 
-  network_interfaces {
-    associate_public_ip_address = true
-  }
+#   network_interfaces {
+#     associate_public_ip_address = true
+#   }
 
   placement {
     availability_zone = "us-west-2a"
