9012ebad04f41f4f6d6c2cc69695453b6c9ea199
Pascal Spielvogel <pascal-spielvogel@gmx.de>
2023-07-25 17:57:57 +0200

    deleted old ALB and ASG

--- auto-scaling-group.tf
+++ auto-scaling-group.tf
@@ -1,16 +1,3 @@
-resource "aws_launch_template" "launch-template" {
-  name                              = "webserver-launch-template"
-  image_id                          = data.aws_ami.amzLinux.id
-  instance_type                     = "t2.micro"
-  vpc_security_group_ids            = [aws_security_group.autoscaling-sg.id]
-  user_data                         = filebase64("stresstest.sh")
-  tag_specifications {
-        resource_type = "instance"
-        tags          = {
-            Name      = "autoserver"
-   }
-  }
-}
 resource "aws_autoscaling_group" "auto-scaling-grp" {
   name                              = "my-autoscaling-group"
   max_size                          = 4
