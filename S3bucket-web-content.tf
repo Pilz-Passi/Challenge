f693e8f5414891cfa92ac3a12c6f43b8c98c2854
Pascal Spielvogel <pascal-spielvogel@gmx.de>
2023-07-26 17:13:58 +0200

    hashed all for later testing

--- S3bucket-web-content.tf
+++ S3bucket-web-content.tf
@@ -1,17 +1,17 @@
-resource "aws_s3_bucket" "wordpress-content-bucket-for-homepage645" {
-  bucket        = "wordpress-content"
-  tags = {
-    Name        = "wordpress-content-bucket-for-homepage645"
-  }
-}
-resource "aws_s3_bucket_ownership_controls" "wordpress-content-bucket-for-homepage645-ownership" {
-  bucket = aws_s3_bucket.wordpress-content-bucket-for-homepage645.id
+# resource "aws_s3_bucket" "wordpress-content-bucket-for-homepage645" {
+#   bucket        = "wordpress-content"
+#   tags = {
+#     Name        = "wordpress-content-bucket-for-homepage645"
+#   }
+# }
+# resource "aws_s3_bucket_ownership_controls" "wordpress-content-bucket-for-homepage645-ownership" {
+#   bucket = aws_s3_bucket.wordpress-content-bucket-for-homepage645.id
 
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-resource "aws_s3_bucket_acl" "wordpress_bucket" {
-  bucket = aws_s3_bucket.wordpress-content-bucket-for-homepage645.id
-  acl    = "private"
-}
+#   rule {
+#     object_ownership = "BucketOwnerPreferred"
+#   }
+# }
+# resource "aws_s3_bucket_acl" "wordpress_bucket" {
+#   bucket = aws_s3_bucket.wordpress-content-bucket-for-homepage645.id
+#   acl    = "private"
+# }
