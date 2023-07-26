20a944d2bf56236e23b5e8d8d49e23483c151ec5
Pascal Spielvogel <pascal-spielvogel@gmx.de>
2023-07-26 10:34:20 +0200

    changed user data

--- Launch-template.tf
+++ Launch-template.tf
@@ -3,7 +3,7 @@ resource "aws_launch_template" "launch-template" {
     image_id                          = data.aws_ami.amzLinux.id
     instance_type                     = "t2.micro"
     vpc_security_group_ids            = [aws_security_group.autoscaling-sg.id]
-    user_data                         = filebase64("stresstest.sh")
+    user_data                         = "${file("stresstest.sh")}"
     tag_specifications {
         resource_type = "instance"
         tags          = {
