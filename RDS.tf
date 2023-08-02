792bd2ecd9703744c096955087d3ad1f98634cf4
Pascal Spielvogel <pascal-spielvogel@gmx.de>
2023-08-02 15:55:22 +0200

    changed db_name

--- RDS.tf
+++ RDS.tf
@@ -5,7 +5,7 @@ resource "aws_db_instance" "WordpressDatabase" {
   engine                 = "${var.engine}"
   engine_version         = "${lookup(var.engine_version, var.engine)}"
   instance_class         = "${var.instance_class}"
-  #name                   = "${var.db_name}"
+  db_name                = "${var.db_name}"
   username               = "${var.username}"
   password               = "${var.password}"
   vpc_security_group_ids = ["${aws_security_group.rds-sg.id}"]
