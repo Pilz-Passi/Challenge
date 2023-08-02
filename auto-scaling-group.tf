51bc17d751926e30e2267a400013cff7a61e93e0
Pascal Spielvogel <pascal-spielvogel@gmx.de>
2023-08-02 14:02:42 +0200

    commented out, because malfuntioning

--- auto-scaling-group.tf
+++ auto-scaling-group.tf
@@ -1,25 +1,25 @@
-resource "aws_autoscaling_group" "auto-scaling-grp" {
-  name                              = "my-autoscaling-group"
-  max_size                          = 4
-  min_size                          = 2
-  desired_capacity                  = 2
-  vpc_zone_identifier               = [aws_subnet.devVPC_public_subnet1.id, aws_subnet.devVPC_public_subnet2.id]
-  target_group_arns                 = [aws_lb_target_group.target-group.arn]
-  health_check_type                 = "ELB"
-  health_check_grace_period         = 500
-  launch_template {
-    id                = aws_launch_template.launch-template.id
-    version           = "$Latest"
-  }
-}
-resource "aws_autoscaling_policy" "policy" {
-  name                              = "CPUpolicy"
-  policy_type                       = "TargetTrackingScaling"
-  target_tracking_configuration {
-    predefined_metric_specification {
-      predefined_metric_type        = "ASGAverageCPUUtilization"
-    }
-      target_value                  = 70.0
-  }
-  autoscaling_group_name            = aws_autoscaling_group.auto-scaling-grp.name
-}
+# resource "aws_autoscaling_group" "auto-scaling-grp" {
+#   name                              = "my-autoscaling-group"
+#   max_size                          = 4
+#   min_size                          = 2
+#   desired_capacity                  = 2
+#   vpc_zone_identifier               = [aws_subnet.devVPC_public_subnet1.id, aws_subnet.devVPC_public_subnet2.id]
+#   target_group_arns                 = [aws_lb_target_group.target-group.arn]
+#   health_check_type                 = "ELB"
+#   health_check_grace_period         = 500
+#   launch_template {
+#     id                = aws_launch_template.launch-template.id
+#     version           = "$Latest"
+#   }
+# }
+# resource "aws_autoscaling_policy" "policy" {
+#   name                              = "CPUpolicy"
+#   policy_type                       = "TargetTrackingScaling"
+#   target_tracking_configuration {
+#     predefined_metric_specification {
+#       predefined_metric_type        = "ASGAverageCPUUtilization"
+#     }
+#       target_value                  = 70.0
+#   }
+#   autoscaling_group_name            = aws_autoscaling_group.auto-scaling-grp.name
+# }
