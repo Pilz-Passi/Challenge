0c9c856f26f79838a6fe25742eae51c9faeae108
Pascal Spielvogel <pascal-spielvogel@gmx.de>
2023-08-02 15:24:09 +0200

    deleted rds-egress

--- sg.tf
+++ sg.tf
@@ -115,11 +115,11 @@ resource "aws_security_group_rule" "rds-sg-in"{
     type                        = "ingress"
     cidr_blocks                 = [var.cidr_blocks]
 }
-resource "aws_security_group_rule" "rds-sg-out"{
-    from_port                   = 3306
-    protocol                    = "tcp"
-    security_group_id           = aws_security_group.rds-sg.id
-    to_port                     = 3306
-    type                        = "egress"
-    cidr_blocks                 = [var.cidr_blocks]
-}
+# resource "aws_security_group_rule" "rds-sg-out"{
+#     from_port                   = 3306
+#     protocol                    = "tcp"
+#     security_group_id           = aws_security_group.rds-sg.id
+#     to_port                     = 3306
+#     type                        = "egress"
+#     cidr_blocks                 = [var.cidr_blocks]
+# }
