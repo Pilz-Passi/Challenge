f1d82fd8f554abe5c8a14c224b9340ce06975eec
Pascal Spielvogel <pascal-spielvogel@gmx.de>
2023-08-03 11:11:33 +0200

    deactivated NACL

--- nacl.tf
+++ nacl.tf
@@ -1,32 +1,41 @@
-# Firewall for the VPC only allowing ingress and egress on ports 80 (http) and 443 (https) 
-# Firewall applies to public subnet 1 and 2
+# # Firewall for the VPC only allowing ingress and egress on ports 80 (http) and 443 (https) 
+# # Firewall applies to public subnet 1 and 2
 
-resource "aws_network_acl" "NACL" {
-  vpc_id = aws_vpc.devVPC.id
-  subnet_ids = [
-    "${aws_subnet.devVPC_public_subnet1.id}",
-    "${aws_subnet.devVPC_public_subnet2.id}",
-  ]
+# resource "aws_network_acl" "NACL" {
+#   vpc_id = aws_vpc.devVPC.id
+#   subnet_ids = [
+#     "${aws_subnet.devVPC_public_subnet1.id}",
+#     "${aws_subnet.devVPC_public_subnet2.id}",
+#   ]
 
-  egress {
-    protocol   = "tcp"
-    rule_no    = 200
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 443
-    to_port    = 443
-  }
+#   egress {
+#     protocol   = "tcp"
+#     rule_no    = 200
+#     action     = "allow"
+#     cidr_block = "0.0.0.0/0"
+#     from_port  = 443
+#     to_port    = 443
+#   }
 
-  ingress {
-    protocol   = "tcp"
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 80
-    to_port    = 80
-  }
+#   ingress {
+#     protocol   = "tcp"
+#     rule_no    = 100
+#     action     = "allow"
+#     cidr_block = "0.0.0.0/0"
+#     from_port  = 80
+#     to_port    = 80
+#   }
 
-  tags = {
-    Name = "NACL"
-  }
-}
+#     egress {
+#     protocol   = "tcp"
+#     rule_no    = 100
+#     action     = "allow"
+#     cidr_block = "0.0.0.0/0"
+#     from_port  = 80
+#     to_port    = 80
+#   }
+
+#   tags = {
+#     Name = "NACL"
+#   }
+# }
